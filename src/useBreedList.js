eda1fb061804c2a049ede9097c5111e238f82cfb
Theo <theo.riberti@gmail.com>
2022-05-06 22:27:45 +0100

    implement SearchParams component using hooks useState, useEffect and useBreedList (custom); updatre eslint to work with hooks

---
+++ src/useBreedList.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+
+const localCache = {}; //this is simplified, we should be using local storage instead here
+
+export default function useBreedList(animal) {
+  const [breedList, setBreedList] = useState([]);
+  const [status, setStatus] = useState("unloaded");
+
+  useEffect(() => {
+    if (!animal) {
+      setBreedList([]);
+    } else if (localCache[animal]) {
+      setBreedList(localCache[animal]);
+    } else {
+      requestBreedList();
+    }
+
+    async function requestBreedList() {
+      setBreedList([]);
+      setStatus("loading");
+      const res = await fetch(
+        `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+      );
+      const json = await res.json();
+      localCache[animal] = json.breeds || [];
+      setBreedList(localCache[animal]);
+      setStatus("loaded");
+    }
+  }, [animal]);
+
+  return [breedList, status];
+}
