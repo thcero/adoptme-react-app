af0a999168e47748421287c6f4d45f96d3ddce2d
Theo <theo.riberti@gmail.com>
2022-05-07 16:14:59 +0100

    add strict mode to app.js, add react router with link tags

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from "react";
 import { render } from "react-dom";
-// import Pet from "./Pet";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { StrictMode } from "react";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-      {/* <Pet name="Luna" animal="dog" breed="Havanese" />
-      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="cat" breed="Mix" /> */}
-    </div>
+    <StrictMode>
+      <BrowserRouter>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </BrowserRouter>
+    </StrictMode>
   );
 };
 
-// render(React.createElement(App), document.getElementById("root"));
 render(<App />, document.getElementById("root"));
